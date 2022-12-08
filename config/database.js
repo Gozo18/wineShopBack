0cc7c9d2fb75087d10a30735d730b021e2d232b5
Roman Jelinek <jelinek82@gmail.com>
2022-12-08 16:29:28 +0100

    Database config

--- config/database.js
+++ config/database.js
@@ -5,7 +5,11 @@ module.exports = ({ env }) => ({
     client: 'sqlite',
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST','0.0.0.0'),
+      port: env.int('DATABASE_PORT','1337'),
+      database: env('DATABASE_NAME','appdb'),
     },
     useNullAsDefault: true,
   },
 });
+ 
